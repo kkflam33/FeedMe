f3f6ec95526cadb17dbe7cf7bfc233e3a25c4dce
Kelvin Lam <kkflam33@gmail.com>
2014-02-24 04:56:49 +0000

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FeedMe::Application.config.secret_key_base = '65d13c422d41ae2e52463b6fff8bdbb01c18da7559c3db9ff37270376168251fc0c508f66539755f0087a5fdeac102b2a663751eb0ba05de7af8610af611ba73'
